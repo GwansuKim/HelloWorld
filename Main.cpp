47a109cc25dce905bbdd36aa0fd35916498540cd
GwansuKim <121069568+GwansuKim@users.noreply.github.com>
2025-09-16 09:08:23 +0900

    Initial commit

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	//하드 코딩 X
+	std::cout << "Hello World" << std::endl << std::endl;
+
+	//keyword -> 외워야 한다
+	//data type(자료형), 정수 자료형
+	//파스칼 케이싱
+	int PrintNum = 1;
+	std::cout << PrintNum << std::endl;
+
+	std::cout << "Commit" << std::endl;
+
+	return 0;
+}
